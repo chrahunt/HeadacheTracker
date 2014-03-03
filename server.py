9de910438e43688390264226b87a53de123e6d03
Bryan Holster <bryanholster@gmail.com>
2014-03-03 21:54:14 +0000

    Added homepage with session variable

--- server.py
+++ server.py
@@ -1,11 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, session, request, escape, redirect, url_for, render_template
 from utils import db_connect
 import MySQLdb 
 app = Flask(__name__)
 
-@app.route("/sample")
-def sample():
-	return render_template("sample.html")
+app.secret_key = 'DS_34*^DS3*90!_ji*217*#NV'
+
+@app.route("/")
+def index():
+  if 'logged' in session:
+    return redirect(url_for('homepage'))
+  return render_template("index.html", selectedNav='Home', loggedIn='false')
+
+@app.route("/login", methods=['GET','POST'])
+def login(): 
+  if 'logged' in session:
+    session.pop('logged', None)
+    return redirect(url_for('index'))
+  return render_template("login.html", selectedNav='Login')
+
+@app.route("/homepage", methods=['GET','POST'])
+def homepage():
+  db = db_connect()
+  cur = db.cursor()
+  
+  if request.method == 'POST':
+    session['logged'] = request.form['username']
+    
+  query = "SELECT * FROM users WHERE username='" + session['logged'] + "'"
+  print query
+  cur.execute(query)
+  
+  userInfo = cur.fetchone()
+  
+  allInfo = {'username' : userInfo[1],
+             'password' : userInfo[2],
+             'firstname' : userInfo[3],
+             'lastname' : userInfo[4]
+             }
+    
+  return render_template("homepage.html", selectedNav='Home', allInfo=allInfo, loggedIn='true')
 
 @app.route("/dbsample")
 def dbsample():
@@ -27,3 +60,4 @@ def dbsample():
 if __name__ == "__main__":
 	app.debug = True
 	app.run(host='0.0.0.0', port=3000)
+  
